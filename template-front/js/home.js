c3f4aed8ac217b96f8650c1a3878b985428a10f5
Alex <ocarneiro.alex@gmail.com>
2018-11-23 00:48:14 -0200

    up

--- template-front/js/home.js
+++ template-front/js/home.js
@@ -95,14 +95,11 @@ $('#carregarValoresNutri').click (function () {
   }
 });
 function signOut() {
-    alert("ok");
-    /*
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
     });
-    window.location.assign("appX/logout.php");
-    */
+    window.location.assign("../appX/logout.php");
 }
 
 
